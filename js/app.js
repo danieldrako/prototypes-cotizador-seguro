b573c40c6bcf7e71866877850bf8b5a3238a4709
danieldrako <ls.ortizdaniel@gmail.com>
2022-11-01 14:09:57 -0600

    after create a prototype to show a message

--- js/app.js
+++ js/app.js
@@ -22,6 +22,30 @@ UI.prototype.llenarOpciones = () => {
         }
 }
 
+// Muestra alertas en pantalla
+UI.prototype.mostrarMensaje = (mensaje, tipo) => {
+
+    const div = document.createElement('div');
+    
+    if(tipo === 'error') {
+        div.classList.add('error');
+    } else {
+        div.classList.add('correcto');
+    }
+
+    div.classList.add('mensaje' , 'mt-10');
+    div.textContent = mensaje;
+
+    // Insertar en el HTML
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.insertBefore(div, document.querySelector('#resultado'));
+
+    setTimeout(() => {
+       div.remove();
+    },3000 )
+}
+
+
 // instanciar UI
 const ui = new UI();
 
@@ -49,11 +73,16 @@ function cotizarSeguro(e) {
     //leer el tipo de cobertura
     const tipo = document.querySelector('input[name="tipo"]:checked').value;
     if(marca === ''|| year === '' || tipo === ''){
-        console.log('no pasó');
-    }else{
-        console.log('si pasó')
+        ui.mostrarMensaje('todos los campos son obligatorios', 'error');
+        return;
     }
- //s
+
+    ui.mostrarMensaje('Cotizando...', 'exito');
+
+    // instanciar el seguro
+
+    // Utilizar el protoype que va a cotizar
+ 
 }
 
 
